840b4e583f9284ba4abb757ee2d30f49e140de96
AlinaGitIgnore <alinazaleska44@gmail.com>
2023-09-05 18:52:37 +0300

    add sorting

--- src/components/Products/index.tsx
+++ src/components/Products/index.tsx
@@ -1,5 +1,7 @@
 import { Product } from '../../redux/types';
-import { useMemo } from 'react';
+import { useEffect, useMemo, useState, useCallback } from 'react';
+import { ReactComponent as SortSvg } from '../../assets/svg/sort-amount-asc.svg';
+import { ReactComponent as FilterSvg } from '../../assets/svg/filter.svg';
 import styled from './index.module.scss';
 
 interface IProps {
@@ -7,45 +9,101 @@ interface IProps {
   query: string;
 }
 
+const tableHeaders = [
+  { label: 'ID', sortable: true, filterable: true },
+  { label: 'Title', sortable: true, filterable: true },
+  { label: 'Description', sortable: true, filterable: true },
+  { label: 'Price', sortable: true, filterable: true },
+  { label: 'Image', sortable: true, filterable: true },
+  { label: 'Rating', sortable: true, filterable: true },
+  { label: 'Stock', sortable: true, filterable: true },
+  { label: 'Category', sortable: true, filterable: true },
+];
+
 const Products = ({ products, query }: IProps) => {
-  const filteredProducts = useMemo(() => {
+  const [ascending, setAccending] = useState(true);
+
+  const searchedProducts = useMemo(() => {
     if (query.length) {
       const normalizeFilter = query.toLowerCase();
 
-      const filteredByCategories = products.filter(
+      const searchedByCategories = products.filter(
         product =>
           product.category &&
           product.category.toLowerCase().includes(normalizeFilter),
       );
 
-      if (!filteredByCategories.length) {
-        const filteredProductsByTitle = products.filter(product => {
+      if (!searchedByCategories.length) {
+        const searchedProductsByTitle = products.filter(product => {
           return product.title.toLowerCase().includes(normalizeFilter);
         });
-        return filteredProductsByTitle;
-      } else return filteredByCategories;
+        return searchedProductsByTitle;
+      } else return searchedByCategories;
     } else return products;
   }, [products, query]);
 
+  const [sortedProducts, setSortedProducts] =
+    useState<Product[]>(searchedProducts);
+
+  // Функция для сортировки списка Product[]
+  const sortProducts = useCallback(
+    (field: keyof Product, ascending: boolean = true) => {
+      const sortedProductsNew = [...searchedProducts];
+
+      sortedProductsNew.sort((a, b) => {
+        if (a && b) {
+          const valueA = a[field];
+          const valueB = b[field];
+
+          if (valueA !== undefined && valueB !== undefined) {
+            if (ascending) {
+              if (valueA < valueB) return -1;
+              if (valueA > valueB) return 1;
+            } else {
+              if (valueA < valueB) return 1;
+              if (valueA > valueB) return -1;
+            }
+          }
+        }
+
+        return 0;
+      });
+
+      setSortedProducts(sortedProductsNew);
+    },
+    [searchedProducts],
+  );
+
+  const handleSort = (label: string) => {
+    setAccending(!ascending);
+    sortProducts(label.toLowerCase() as keyof Product, ascending);
+  };
+
+  useEffect(() => {
+    sortProducts('id');
+  }, [sortProducts]);
+
   return (
     <div className={styled.productsWrapper}>
-      {' '}
       <table className={styled.productTable}>
         <thead className={styled.tableHeader}>
           <tr>
-            <th>ID</th>
-            <th>Title</th>
-            <th>Description</th>
-            <th>Price</th>
-            <th>Image</th>
-            <th>Rating</th>
-            <th>Stock</th>
-            <th>Category</th>
+            {tableHeaders.map(header => (
+              <th key={header.label}>
+                <span>{header.label}</span>
+                <div className={styled.actions}>
+                  {header.label !== 'Image' && (
+                    <SortSvg onClick={() => handleSort(header.label)} />
+                  )}
+                  <FilterSvg />
+                </div>
+              </th>
+            ))}
           </tr>
         </thead>
 
         <tbody className={styled.productsTableBody}>
-          {filteredProducts.map(product => (
+          {sortedProducts.map(product => (
             <tr className={styled.productItem} key={product.id}>
               <td>{product.id}</td>
               <td>{product.title}</td>
